b8fed2086eb86b97f43d3c84201e3d1f19af59ed
Dzulfikar Budi Andreanto <139447405+budizulkoplo@users.noreply.github.com>
2025-10-28 14:11:49 +0700

    update ui

--- resources/views/mobile/index.blade.php
+++ resources/views/mobile/index.blade.php
@@ -132,7 +132,7 @@
         </div>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+        <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Tutup</button>
       </div>
     </div>
   </div>
