103b3b6faf0f7e99b8fe6dd2f48079456dc50354
Dzulfikae Budi Andreanto <buddyandreanto@gmail.com>
2026-03-14 12:31:22 +0700

    nota toko

---
+++ resources/views/transaksi/toko/invoice_pdf.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+body{
+    font-family: DejaVu Sans, sans-serif;
+    font-size:12px;
+}
+
+table{
+    width:100%;
+    border-collapse: collapse;
+}
+
+th,td{
+    border-bottom:1px solid #ddd;
+    padding:6px;
+}
+
+.header{
+    margin-bottom:20px;
+}
+
+.right{
+    text-align:right;
+}
+
+.total{
+    margin-top:20px;
+}
+
+.footer{
+    margin-top:40px;
+}
+</style>
+</head>
+<body>
+
+<table class="header">
+<tr>
+<td width="70%">
+<h3>INVOICE</h3>
+
+<strong>Kepada</strong><br>
+{{ $nota->customerToko->nama_lengkap ?? '-' }}<br>
+{{ $nota->customerToko->alamat ?? '' }}<br>
+{{ $nota->customerToko->no_hp ?? '' }}
+
+</td>
+
+<td class="right">
+
+@if($logoPath)
+<img src="{{ $logoPath }}" height="60">
+@endif
+
+<br><br>
+
+Nomor : {{ $nota->nota_no }} <br>
+Tanggal : {{ $nota->tanggal }} <br>
+Jatuh Tempo : {{ $nota->tgl_tempo }}
+
+</td>
+</tr>
+</table>
+
+<table>
+<thead>
+<tr>
+<th>#</th>
+<th>Deskripsi</th>
+<th>Vol</th>
+<th>Satuan</th>
+<th>Harga</th>
+<th>Total</th>
+</tr>
+</thead>
+
+<tbody>
+@foreach($nota->transactions as $i => $trx)
+<tr>
+<td>{{ $i+1 }}</td>
+<td>{{ $trx->description }}</td>
+<td>{{ $trx->jml }}</td>
+<td>{{ $trx->satuan ?? '-' }}</td>
+<td class="right">Rp {{ number_format($trx->nominal,0,',','.') }}</td>
+<td class="right">Rp {{ number_format($trx->total,0,',','.') }}</td>
+</tr>
+@endforeach
+</tbody>
+</table>
+
+<table class="total">
+<tr>
+<td width="80%" class="right"><strong>Total</strong></td>
+<td class="right">
+<strong>Rp {{ number_format($nota->total,0,',','.') }}</strong>
+</td>
+</tr>
+</table>
+
+<div class="footer">
+Mengetahui,
+<br><br><br>
+
+________________________
+</div>
+
+</body>
+</html>
